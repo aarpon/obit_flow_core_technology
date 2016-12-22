9a568d312a7dc10a9303db84619dbf6caecf59b0
Aaron Ponti <aaron.ponti@bsse.ethz.ch>
2016-12-22 12:01:09 +0100

    Remove duplicate declaration of CONFIG.

--- core-plugins/flow/1/as/webapps/bdfacsdiva-viewer/html/js/config/config.js
+++ core-plugins/flow/1/as/webapps/bdfacsdiva-viewer/html/js/config/config.js
@@ -2,9 +2,6 @@
  * This is the default (fallback) configuration file for the web application.
  */
 
-// Create empty CONFIG object as fallback if it does not exist.
-var CONFIG = CONFIG || {};
-
 // Default configuration.
 var DEFAULT_CONFIG = {
 
