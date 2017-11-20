4fc13b1cd73a682c2c53e160ff67bd5d47460d2a
Aaron Ponti <aarpon@bsse.ethz.ch>
2017-11-20 09:16:06 +0100

    Minor change in imports.

--- core-plugins/flow/1/dss/reporting-plugins/retrieve_fcs_events/retrieve_fcs_events.py
+++ core-plugins/flow/1/dss/reporting-plugins/retrieve_fcs_events/retrieve_fcs_events.py
@@ -20,12 +20,12 @@ import os.path
 import logging
 import java.io.File
 import java.util.ArrayList
-import ch.ethz.scu.obit.bdfacsdivafcs.readers.FCSReader as FCSReader
-import ch.ethz.scu.obit.bdfacsdivafcs.readers.Hyperlog as Hyperlog
 import json
-from ch.ethz.scu.obit.common.server.longrunning import LRCache
 import uuid
 from threading import Thread
+from ch.ethz.scu.obit.flow.readers import FCSReader
+from ch.ethz.scu.obit.flow.readers import Hyperlog
+from ch.ethz.scu.obit.common.server.longrunning import LRCache
 
 
 def setUpLogging():
