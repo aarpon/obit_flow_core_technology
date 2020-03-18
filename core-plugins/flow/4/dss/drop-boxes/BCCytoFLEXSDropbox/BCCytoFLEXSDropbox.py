4d93a88ead2c45b4c956815760ee20f110900002
Aaron Ponti <aaron.ponti@bsse.ethz.ch>
2020-03-18 13:32:39 +0100

    Add BC CytoFLEX S dropbox.

---
+++ core-plugins/flow/4/dss/drop-boxes/BCCytoFLEXSDropbox/BCCytoFLEXSDropbox.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: Aaron Ponti
+"""
+
+from Processor import Processor
+
+
+def process(transaction):
+    """Dropbox entry point.
+
+    @param transaction, the transaction object
+    """
+
+    #
+    # Run registration
+    #
+    prefix = "CYTOFLEX_S"
+    version = 2
+    logDir = "../core-plugins/flow/4/dss/drop-boxes/BDCytoFLEXSDropbox/logs"
+
+    processor = Processor(transaction, prefix, version, logDir)
+    processor.run()
